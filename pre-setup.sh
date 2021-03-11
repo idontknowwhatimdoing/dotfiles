6c53cf735ab5c1870c09e2fff758fb6a00f53758
jkromer <jkromer@student.42.fr>
2021-03-11 19:28:14 +0100

    menfou

--- pre-setup.sh
+++ pre-setup.sh
@@ -8,14 +8,13 @@ makepkg -si
 
 yay -S --needed - < pkglist.txt
 
-mkdir -p ~/dowloads
+mkdir -p ~/downloads
 cd ~/downloads
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraMono.zip
 unzip FiraMono.zip
 rm -rf FiraMono.zip *Windows*
 sudo mkdir -p /usr/share/fonts/FiraMono
 sudo mv * /usr/share/fonts/FiraMono
-fc-cache
 
 cd
 ./setup.sh
