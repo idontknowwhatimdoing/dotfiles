c433c4139efeaffbbac12c89da672b49f53dfc1d
jkromer <jkromer@student.42.fr>
2021-05-02 15:58:02 +0200

    set-theme + readme

--- README.md
+++ README.md
@@ -1,13 +1,32 @@
 # dotfiles
 Well, here are my dotfiles ...
+- uya theme:
 ![showcase](merge_from_ofoct.png)
+
+## Installation
+```
+git clone https://github.com/idontknowwhatimdoing/dotfiles ~/.dotfiles
+cd ~/.dotfiles
+```
+then<br>
+```
+./set-theme.sh <theme>
+```
+or just
+```
+./set-theme.sh
+``` (if you have fzf installed)<br>
+
+themes available for now:
+* uya (see showcase image)
+* nord (work in progress)
 
 ## Scripts
 I have a few scripts, here they are:
 
 ### pre-setup.sh
 Installs all dependencies listed in pkglist.txt with pacman (and yay) as well as [Fira Mono Nerd Font](https://www.nerdfonts.com/font-downloads) and creates the config directories.<br>
-You most likely don't need to use it, but if you do: `./pre-setup.sh` (requires sudo and git to be installed)
+It is desinged to be used after a fresh arch install so you most likely don't need to use it, but if you do: `./pre-setup.sh` (requires sudo and git to be installed)
 
 ### set-theme.sh
 Sets the theme (found in the themes directory) and links all config files<br>
--- set-theme.sh
+++ set-theme.sh
@@ -1,5 +1,32 @@
 #!/usr/bin/bash
 
+which fzf >/dev/null
+if [[ $? -eq 0 ]];then
+	theme=$(ls ~/.dotfiles/themes | fzf --disabled --preview-window=hidden)
+
+	if [[ -n $theme ]]; then
+		ln -f themes/$theme/alacritty/alacritty.yml ~/.config/alacritty
+		ln -f themes/$theme/tmux/tmux.conf ~/.config/tmux/tmux.conf
+		ln -f themes/$theme/nvim/init.vim ~/.config/nvim
+		ln -f themes/$theme/nvim/plugins.vim ~/.config/nvim
+		ln -f themes/$theme/nvim/$theme.vim ~/.config/nvim/colors
+		ln -f themes/$theme/i3/config ~/.config/i3
+		ln -f themes/$theme/polybar/config ~/.config/polybar
+		ln -f themes/$theme/polybar/launch.sh ~/.config/polybar/scripts
+		ln -f themes/$theme/polybar/check-updates.sh ~/.config/polybar/scripts
+		ln -f themes/$theme/zsh/zshrc ~/.zshrc
+		ln -f themes/$theme/rofi/config.rasi ~/.config/rofi
+		ln -f themes/$theme/rofi/$theme.rasi ~/.config/rofi
+		dir=$(echo -n ~/.mozilla/firefox/ ; grep -i default ~/.mozilla/firefox/installs.ini | awk -F= '{print $2}')
+		ln -f themes/$theme/firefox/user.js $dir
+		cp -r themes/$theme/firefox/* $dir/chrome
+
+		source ~/.zshrc
+		ln -f xorg/xinitrc ~/.xinitrc
+	fi
+	exit
+fi
+
 theme=$1
 
 if [[ $# -eq 1 ]]; then
